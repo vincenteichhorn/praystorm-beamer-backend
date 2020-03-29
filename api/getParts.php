4e64c840a0f215bf56ae650be5c81efcaf173fb3
Vincent Eichhorn <vincent.cuvei@gmail.com>
2020-03-29 13:53:58 +0200

    added request methods, status to getParts

--- api/getParts.php
+++ api/getParts.php
@@ -1,21 +1,27 @@
 <?php
   header("Access-Control-Allow-Origin: *");
-  header("Access-Control-Allow-Headers: *");
-  header("Content-Type: application/json; charset=UTF-8");
-
-  require 'config/Database.php';
-  $database = new Database();
-  $data = $database->getParts($_POST['name'], $_POST['date']);
-  $list = array();
-  foreach($data as $row){ 
-    $list[] = array(
-      "title"=>$row['title'],
-      "position"=>$row['position'],
-      "type"=>$row['type'],
-      "author"=>$row['author'],
-      "album"=>$row['album'],
-      "copyright"=>$row['copyright']
-    );    
+  require "enumnerations/Enumerations.php";
+  if($_SERVER["REQUESR_METHOD" == RequestMethods::POST]) {
+    if(isset($_POST['name']) && isset($_POST['date'])) {
+      require 'config/Database.php';
+      $database = new Database();
+      $data = $database->getParts($_POST['name'], $_POST['date']);
+      $list = array();
+      foreach($data as $row){ 
+        $list[] = array(
+          "title"=>$row['title'],
+          "position"=>$row['position'],
+          "type"=>$row['type'],
+          "author"=>$row['author'],
+          "album"=>$row['album'],
+          "copyright"=>$row['copyright']
+        );    
+      }
+      echo json_encode($list);
+    } else {
+      header(RequestStatus::badRequest);
+    }
+  } else {
+    header(RequestStatus::methodNotAllowed);
   }
-  echo json_encode($list);
 ?>
