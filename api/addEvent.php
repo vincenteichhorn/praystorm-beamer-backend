1cd919c1b8a1fe4a706d2730aeda0b9e14ff0f45
Vincent Eichhorn <vincent.cuvei@gmail.com>
2020-10-18 12:33:49 +0200

    changed addEvent req method error

--- api/addEvent.php
+++ api/addEvent.php
@@ -1,17 +1,26 @@
 <?php
-$name = $_POST['name'];
-$date = $_POST['date'];
-$desc = $_POST['description'];
-
-require 'config/Database.php';
-$database = new Database();
-$condition = array(
-    array($name),
-    array($date)
-);
-if($database->countElements("events",$condition)==0){
-    $database->addEvent(array("name"=>$name, "date"=>$date, "description"=>$desc));
-}else{
-    echo "Event existiert bereits";
-}
+header("Access-Control-Allow-Origin: *");
+require "enumerations/Enumerations.php";
+if($_SERVER["REQUEST_METHOD"] == RequestMethods::POST) {
+    if(isset($_POST['name']) && isset($_POST['date']) && isset($_POST['description'])) {
+      require 'config/Database.php';
+      $database = new Database();
+      $name = $_POST['name'];
+      $date = $_POST['date'];
+      $desc = $_POST['description'];
+      $condition = array(
+        array($name),
+        array($date)
+        );
+        if($database->countElements("events", $condition) == 0){
+            $database->addEvent(array("name" => $name, "date" => $date, "description" => $desc));
+        } else {
+            echo "Event existiert bereits";
+        }
+    } else {
+      header(RequestStatus::badRequest);
+    }
+} else {
+header(RequestStatus::methodNotAllowed);
+}   
 ?>
