613e43f4e21d085a4feff159d59531041394401b
Vincent Eichhorn <vincent.cuvei@gmail.com>
2020-10-31 13:54:40 +0100

    addPart controller

---
+++ api/addPart.php
@@ -0,0 +1,44 @@
+<?php
+$data = (array) json_decode($_POST['data'], true);
+
+require 'db_handling/Database.php';
+$database = new Database();
+foreach($data as $row){
+    $condition = array(
+        array("title",$row['title'])
+    );
+    if($database->countElements("parts",$condition)==0){
+        $database->addPart($row);
+    }else{
+        echo "Part existiert bereits";
+    }
+}
+
+header("Access-Control-Allow-Origin: *");
+require "enumerations/Enumerations.php";
+if($_SERVER["REQUEST_METHOD"] == RequestMethods::POST) {
+    if(isset($_POST['title']) && isset($_POST['type']) && isset($_POST['author']) && isset($_POST['album']) && isset($_POST['copyright']) && isset($_POST['eventName'])) {
+        require 'db_handling/Database.php';
+        $database = new Database();
+        $title = $_POST['title'];
+        $type = $_POST['type'];
+        $author = $_POST['author'];
+        $album = $_POST['album'];
+        $copyright = $_POST['copyright'];
+        $condition = array(
+            array("title", $title)
+        );
+        if($database->countElements("parts",$condition)==0){
+            //$database->addPart($row);
+            //TODO: add to database
+        } else {
+            header(RequestStatus::badRequest);
+        }
+    } else {
+      header(RequestStatus::badRequest);
+    }
+} else {
+    header(RequestStatus::methodNotAllowed);
+}   
+
+?>
